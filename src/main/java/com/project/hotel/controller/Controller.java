e9dbd8d9293d7d642001644ed0c44d66e320be3b
yudonggeun <ydong98@gmail.com>
2023-10-24 17:09:36 +0900

    refactor : controller 수정

--- src/main/java/com/project/hotel/controller/Controller.java
+++ src/main/java/com/project/hotel/controller/Controller.java
@@ -1,33 +1,30 @@
 package com.project.hotel.controller;
 
 import com.project.hotel.common.SessionManager;
-import com.project.hotel.domain.Person;
-import com.project.hotel.input.Input;
-import com.project.hotel.output.Output;
-import com.project.hotel.output.response.ErrorResponse;
+import com.project.hotel.output.response.DeleteReservationResponse;
+import com.project.hotel.output.response.GetReservationResponse;
+import com.project.hotel.output.response.ReserveResponse;
 import com.project.hotel.service.HotelService;
 
 public class Controller {
 
-    private final Input input;
-    private final Output output;
     private final HotelService hotel;
     private final SessionManager sessionManager;
 
-    public Controller(Input input, Output output, HotelService hotel, SessionManager sessionManager) {
-        this.input = input;
-        this.output = output;
+    public Controller(HotelService hotel, SessionManager sessionManager) {
         this.hotel = hotel;
         this.sessionManager = sessionManager;
     }
 
-    public Object reserve(String roomId, String userId, String password){
-        try {
-            Person user = sessionManager.getUser(userId, password);
-            var data = hotel.reserve(user);
-            return null;
-        } catch (Exception e){
-            return new ErrorResponse(e.getMessage());
-        }
+    public ReserveResponse reserve(String roomId, String userId, String password) {
+        return null;
+    }
+
+    public DeleteReservationResponse deleteReservation(String reservationId, String userId, String password){
+        return null;
+    }
+
+    public GetReservationResponse getReservationList(String userId, String password){
+        return null;
     }
 }
