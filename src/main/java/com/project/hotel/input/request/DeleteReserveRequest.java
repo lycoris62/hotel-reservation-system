81ddd954ad922df9fded196f4ebd6277a0ef244d
barami62 <wodbs7758@naver.com>
2023-10-24 17:48:56 +0900

    fix : 호텔이 받는 예약 관련 DTO 추가

--- src/main/java/com/project/hotel/input/request/DeleteReserveRequest.java
+++ src/main/java/com/project/hotel/input/request/DeleteReserveRequest.java
@@ -1,6 +1,9 @@
 package com.project.hotel.input.request;
 
+import com.project.hotel.domain.Person;
+
 public record DeleteReserveRequest(
-        String reservationId
+	Person person,
+	String reservationId
 ) {
 }
