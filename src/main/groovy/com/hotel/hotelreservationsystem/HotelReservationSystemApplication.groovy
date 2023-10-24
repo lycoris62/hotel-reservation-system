692380dfd0196598c0a652339347df2ae0f5bba9
yudonggeun <ydong98@gmail.com>
2023-10-24 12:28:06 +0900

    create project

---
+++ src/main/groovy/com/hotel/hotelreservationsystem/HotelReservationSystemApplication.groovy
@@ -0,0 +1,13 @@
+package com.hotel.hotelreservationsystem
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HotelReservationSystemApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HotelReservationSystemApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/hotel/hotelreservationsystem/HotelReservationSystemApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.hotel.hotelreservationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
