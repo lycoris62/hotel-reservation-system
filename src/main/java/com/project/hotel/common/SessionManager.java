56743c0e4ccecf181bd8e57f387c9e943c73d3ae
jump6923 <jump6923@gmail.com>
2023-10-24 17:28:05 +0900

    feat : SessionManager 구현, Person 소지금 추가

--- src/main/java/com/project/hotel/common/SessionManager.java
+++ src/main/java/com/project/hotel/common/SessionManager.java
@@ -1,10 +1,24 @@
 package com.project.hotel.common;
 
 import com.project.hotel.domain.Person;
+import com.project.hotel.domain.Role;
+
+import java.util.HashMap;
 
 public class SessionManager {
+    HashMap<String, Person> userMap = new HashMap<>();
 
-    public Person getUser(String id, String password){
-        return null;
+    SessionManager() {
+        Person admin = new Person("admin", "123", "010-0000-0000", "관리자", Role.ADMIN, 50000);
+        Person customer1 = new Person("user1", "111", "010-1111-1111", "투숙객", Role.CUSTOMER, 300000);
+        Person customer2 = new Person("user2", "222", "010-2222-2222", "장발장", Role.CUSTOMER, 300);
+        userMap.put(admin.getId(), admin);
+        userMap.put(customer1.getId(), customer1);
+        userMap.put(customer2.getId(), customer2);
     }
+
+    public Person getUser(String id, String password) {
+        return userMap.get(id);
+    }
+
 }
