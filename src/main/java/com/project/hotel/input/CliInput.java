6d11cd2db80eba425d88d6bc8972bf556f60f1aa
yudonggeun <ydong98@gmail.com>
2023-10-24 20:17:11 +0900

    feat : main 실행시 절차를 분리

--- src/main/java/com/project/hotel/input/CliInput.java
+++ src/main/java/com/project/hotel/input/CliInput.java
@@ -1,12 +1,9 @@
 package com.project.hotel.input;
 
-import com.project.hotel.output.CliOutput;
-
 public class CliInput implements Input{
     public static CliInput create() {
         return new CliInput();
     }
-
     @Override
     public String command() {
         return null;
