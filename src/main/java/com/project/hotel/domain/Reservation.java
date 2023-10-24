e120887118a516d6ff61025153ef775438b9b9f8
barami62 <wodbs7758@naver.com>
2023-10-24 16:21:39 +0900

    fix : 예약 날짜에 시간 추가

--- src/main/java/com/project/hotel/domain/Reservation.java
+++ src/main/java/com/project/hotel/domain/Reservation.java
@@ -1,29 +1,28 @@
 package com.project.hotel.domain;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 // 4. 예약은 객실, 고객의 이름, 고객의 전화번호,예약 날짜를 가지고 있다. + id(String UUID)도 가진다.
 public class Reservation {
 
-    private String id;
-    private Room room;
-    private Person person;
+	private String id;
+	private Room room;
+	private Person person;
+	private LocalDateTime dateTime;
 
-    private LocalDate date;
+	public String getId() {
+		return id;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public Room getRoom() {
+		return room;
+	}
 
-    public Room getRoom() {
-        return room;
-    }
+	public Person getPerson() {
+		return person;
+	}
 
-    public Person getPerson() {
-        return person;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
+	public LocalDateTime getDate() {
+		return dateTime;
+	}
 }
