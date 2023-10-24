d7f49da61e12cf739869292cd58cf7832155340f
yudonggeun <ydong98@gmail.com>
2023-10-24 15:12:01 +0900

    feat : room 도메인 getter 구현

--- src/main/java/com/project/hotel/domain/Room.java
+++ src/main/java/com/project/hotel/domain/Room.java
@@ -6,5 +6,22 @@ public class Room {
     private LocalDate date;
     private Person person;
     private double area;
+
     private int fee;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public int getFee() {
+        return fee;
+    }
 }
