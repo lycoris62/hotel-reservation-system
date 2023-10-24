15d27e7f55478abc13bf5f984496240b70877e98
barami62 <wodbs7758@naver.com>
2023-10-24 17:33:53 +0900

    fix : 호텔서비스 인터페이스 수정

--- src/main/java/com/project/hotel/service/HotelService.java
+++ src/main/java/com/project/hotel/service/HotelService.java
@@ -1,5 +1,6 @@
 package com.project.hotel.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.project.hotel.domain.Person;
@@ -8,12 +9,13 @@ import com.project.hotel.domain.Reservation;
 public interface HotelService {
 
 	/**
-	 * 고객이 예약할 때 사용.
+	 * 고객이 예약할 때 사용. 예약 성공시 예약 번호 반환, 실패시 "fail" 반환
 	 *
 	 * @param person 고객
-	 * @return boolean 예약 실패시 false 반환
+	 * @param dateTime 원하는 예약 날짜
+	 * @return String
 	 */
-	boolean reserve(Person person);
+	String reserve(Person person, LocalDateTime dateTime);
 
 	/**
 	 * 사용자의 예약 정보 조회할 때 사용. 고객이면 고객의 예약 내역, 관리자면 전체 예약 내역을 반환.
