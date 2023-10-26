fffa613f38c7398cfc2b5155f5d57726e47e0e68
jump6923 <jump6923@gmail.com>
2023-10-26 11:35:42 +0900

    feat : ReserveRequest roomId 추가

--- src/main/java/com/project/hotel/controller/Dispatcher.java
+++ src/main/java/com/project/hotel/controller/Dispatcher.java
@@ -1,5 +1,9 @@
 package com.project.hotel.controller;
 
+import com.project.hotel.input.request.DeleteReserveRequest;
+import com.project.hotel.input.request.GetReserveListRequest;
+import com.project.hotel.input.request.ReserveRequest;
+
 public class Dispatcher {
 
     private Controller controller;
@@ -8,7 +12,32 @@ public class Dispatcher {
         this.controller = controller;
     }
 
-    public Object handle(Object request){
-        return null;
+    public Object handle(Object request) {
+        if (request instanceof ReserveRequest reserveRequest) {
+            var user = reserveRequest.person();
+            var id = user.getId();
+            var password = user.getPassword();
+            var roomId = reserveRequest.roomId();
+            var dateTime = reserveRequest.dateTime();
+            return controller.reserve(roomId, id, password);
+        }
+
+        if (request instanceof GetReserveListRequest getReserveListRequest) {
+            var user = getReserveListRequest.person();
+            var id = user.getId();
+            var password = user.getPassword();
+            return controller.getReservationList(id, password);
+        }
+
+        if (request instanceof DeleteReserveRequest deleteReserveRequest) {
+            var user = deleteReserveRequest.person();
+            var id = user.getId();
+            var password = user.getPassword();
+            var reservation = deleteReserveRequest.reservationId();
+            return controller.deleteReservation(id, password, reservation);
+        }
+
+
+        throw new RuntimeException("값 오류");
     }
 }
