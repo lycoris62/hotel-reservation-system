45c7cc19737a86e321fb8c74b6987a353fea9abe
barami62 <wodbs7758@naver.com>
2023-10-26 11:34:08 +0900

    feat : 예약 도메인 생성자 추가

--- src/main/java/com/project/hotel/domain/Reservation.java
+++ src/main/java/com/project/hotel/domain/Reservation.java
@@ -1,6 +1,7 @@
 package com.project.hotel.domain;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 // 4. 예약은 객실, 고객의 이름, 고객의 전화번호,예약 날짜를 가지고 있다. + id(String UUID)도 가진다.
 public class Reservation {
@@ -9,8 +10,16 @@ public class Reservation {
 	private Room room;
 	private Person person;
 	private LocalDateTime dateTime;
-
 	private int price;
+
+	public Reservation(Room room, Person person, LocalDateTime dateTime, int price) {
+		this.id = UUID.randomUUID().toString().split("-")[0];
+		this.room = room;
+		this.person = person;
+		this.dateTime = dateTime;
+		this.price = price;
+	}
+
 
 	public String getId() {
 		return id;
