200441595478773de48b22b7a4315c2efecd7a52
yudonggeun <ydong98@gmail.com>
2023-10-26 12:43:02 +0900

    bug : system.in 스트림 중복 사용 버그 수정

--- src/main/java/com/project/hotel/input/CliInput.java
+++ src/main/java/com/project/hotel/input/CliInput.java
@@ -2,17 +2,25 @@ package com.project.hotel.input;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class CliInput implements Input {
+
+    private static CliInput cliInput;
     public static CliInput create() {
-        return new CliInput();
+        if(cliInput == null) return cliInput = new CliInput(System.in);
+        return cliInput;
     } //팩토리 매서드
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public CliInput(InputStream stream) {
+        br = new BufferedReader(new InputStreamReader(stream));
+    }
+
+    private final BufferedReader br;
 
     @Override
     public String command() throws IOException {
-        String s = br.readLine();
-        return s;
+        return br.readLine();
     }
 }
