7077ab36ab3c31ab61cef6fa687d326e7cff82bb
barami62 <wodbs7758@naver.com>
2023-10-26 11:38:03 +0900

    feat : 호텔 예약 기능 추가

--- src/main/java/com/project/hotel/service/Hotel.java
+++ src/main/java/com/project/hotel/service/Hotel.java
@@ -1,26 +1,74 @@
 package com.project.hotel.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import com.project.hotel.domain.Person;
 import com.project.hotel.domain.Reservation;
 import com.project.hotel.domain.Role;
+import com.project.hotel.domain.Room;
 import com.project.hotel.input.request.DeleteReserveRequest;
 import com.project.hotel.input.request.GetReserveListRequest;
 import com.project.hotel.input.request.ReserveRequest;
 import com.project.hotel.output.response.DeleteReservationResponse;
 import com.project.hotel.output.response.GetReservationResponse;
 import com.project.hotel.output.response.ReserveResponse;
+import com.project.hotel.repository.AssetDb;
+import com.project.hotel.repository.AssetRepository;
 import com.project.hotel.repository.ReservationRepository;
+import com.project.hotel.repository.ReservationRepositoryImpl;
+import com.project.hotel.repository.RoomDb;
+import com.project.hotel.repository.RoomRepository;
 
 public class Hotel implements HotelService {
 
-    private ReservationRepository reservationRepository;
+	private final ReservationRepository reservationRepository = new ReservationRepositoryImpl();
+	private final RoomRepository roomRepository = new RoomDb();
+	private final AssetRepository assetRepository = new AssetDb();
 
-    @Override
-    public ReserveResponse reserve(ReserveRequest request) {
-        return null;
-    }
+	@Override
+	public ReserveResponse reserve(ReserveRequest request) {
+		Person user = request.person();
+		LocalDateTime reservationDateTime = request.dateTime();
+		LocalDate reservationDate = reservationDateTime.toLocalDate();
+
+		if (!user.getRole().equals(Role.CUSTOMER)) {
+			return new ReserveResponse(null, true);
+		}
+
+		List<Room> availableRoomList = getAvailableRoomList(user, reservationDate);
+
+		if (availableRoomList.isEmpty()) {
+			return new ReserveResponse(null, true);
+		}
+
+		Reservation reservation = processReservation(availableRoomList, user, reservationDateTime);
+
+		return new ReserveResponse(reservation, false);
+	}
+
+	private List<Room> getAvailableRoomList(Person user, LocalDate reservationDate) {
+		return roomRepository.findRoomMoney(user.getMoney())
+			.stream()
+			.filter(room -> !room.isReserved(reservationDate))
+			.toList();
+	}
+
+	private Reservation processReservation(List<Room> availableRoomList, Person user,
+		LocalDateTime reservationDateTime) {
+		Room room = availableRoomList.get(0);
+		payForRoomFee(user, room);
+		room.reserve(reservationDateTime.toLocalDate());
+
+		return new Reservation(room, user, reservationDateTime, room.getFee());
+	}
+
+	private void payForRoomFee(Person user, Room room) {
+		user.minusMoney(room.getFee());
+		assetRepository.plus(room.getFee());
+	}
 
     @Override
     public GetReservationResponse getReservations(GetReserveListRequest request) {
